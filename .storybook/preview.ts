9ca59ee81ce20cb4720577a02bad231eb18095bc
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-05-23 22:25:48 +0200

    chore: setup storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,22 @@
+import 'tailwindcss/tailwind.css'
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+		options: {
+			storySort: (a, b) =>
+				a.id === b.id
+					? 0
+					: a.id.localeCompare(b.id, undefined, { numeric: true }),
+		},
+	},
+}
+
+export default preview
